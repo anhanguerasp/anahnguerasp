e8debc9eb38ffe4f956ab9746208c89873b8748b
anhanguerasp <posanhanguerasp@gmail.com>
2023-09-26 15:13:15 -0300

    scrool down button on unity pages

--- JS/script.js
+++ JS/script.js
@@ -1021,3 +1021,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
     texto16.style.display = "list-item";
   });
 });
+
+function scrollDown() {
+  // Obtém a posição atual da janela
+  const currentPosition = window.scrollY;
+
+  // Define a posição de destino (pode ajustar essa parte de acordo com sua necessidade)
+  const targetPosition = currentPosition + 300; // Role 300 pixels para baixo
+
+  // Anima a rolagem suavemente
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth", // Essa opção faz a rolagem ser suave
+  });
+}
